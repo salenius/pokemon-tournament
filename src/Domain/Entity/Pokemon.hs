06b7b5ac4fde6d997a3a5d7d02d80df05bac6b3c
Tommi Salenius <tommisalenius@gmail.com>
2021-02-25 00:53:12 +0200

    Sovitettu uuden PokemonSpeciesin mukaan

--- src/Domain/Entity/Pokemon.hs
+++ src/Domain/Entity/Pokemon.hs
@@ -1,8 +1,8 @@
-{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TemplateHaskell, RankNTypes, GADTs #-}
 
 module Domain.Entity.Pokemon where
 
-import Domain.Entity.Pokemon.Species
+import Domain.Entity.Pokemon.Species as PS
 import Domain.Attribute.Ability
 import Domain.Attribute.Statistic
 import Domain.Attribute.HeldItem
@@ -16,7 +16,7 @@ data Quadruple m =
   | Duoruple m m
   | Triruple m m m
   | Quadruple m m m m
-  deriving (Eq)
+  deriving (Eq,Read)
 
 instance Show m => Show (Quadruple m) where
   show (Monoruple a) = show [a]
@@ -30,30 +30,54 @@ instance Functor Quadruple where
   fmap f (Triruple a b c) = Triruple (f a) (f b) $ f c
   fmap f (Quadruple a b c d) = Quadruple (f a) (f b) (f c) $ f d
 
+data PokemonSpecies' =
+  forall pkmn. PokemonSpecies pkmn => PokemonSpecies' { getSpecies :: pkmn}
+
+instance Eq PokemonSpecies' where
+  (PokemonSpecies' p) == (PokemonSpecies' p') = 
+    baseHp p == baseHp p' &&
+    baseAttack p == baseAttack p' &&
+    baseDefence p == baseDefence p' &&
+    baseSAttack p == baseSAttack p' &&
+    baseSDefence p == baseSDefence p' &&
+    baseSpeed p == baseSpeed p' &&
+    weight p == weight p' &&
+    typeOfPokemon p == typeOfPokemon p' &&
+    genderRatio p == genderRatio p' &&
+    (show $ revertToForm p) == (show $ revertToForm p') &&
+    possibleAbilities p == possibleAbilities p' &&
+    isPikachu p == isPikachu p'
+
+instance Show PokemonSpecies' where
+  show (PokemonSpecies' p) = show p
+
 data Pokemon m = Pokemon
   {
-    _species :: PokemonSpecies
-  , _possibleAbilities :: [Ability]
+    _species :: PokemonSpecies'
   , _possibleHeldItem :: HeldItem
   , _moves :: Quadruple m
   , _pokemonLevel :: Level'
   , _pokemonNature :: Nature
   , _pokemonEvs :: EVs
   , _pokemonIvs :: IVs
-  } deriving (Eq)
+  } deriving (Eq,Show)
 
 makeLenses ''Pokemon
 
-instance Functor Pokemon where
-  fmap f pkmn = over moves (fmap f) pkmn
+instance PokemonSpecies PokemonSpecies' where
+  baseHp (PokemonSpecies' p) = baseHp p
+  baseAttack (PokemonSpecies' p) = baseAttack p
+  baseDefence (PokemonSpecies' p) = baseDefence p
+  baseSAttack (PokemonSpecies' p) = baseSAttack p
+  baseSDefence (PokemonSpecies' p) = baseSDefence p
+  baseSpeed (PokemonSpecies' p) = baseSpeed p
+  weight (PokemonSpecies' p) = weight p
+  typeOfPokemon (PokemonSpecies' p) = typeOfPokemon p
+  genderRatio (PokemonSpecies' p) = genderRatio p
+  possibleAbilities (PokemonSpecies' p) = possibleAbilities p
+  revertToForm (PokemonSpecies' p) = PokemonSpecies' $ revertToForm p
+  isPikachu (PokemonSpecies' p) = isPikachu p
 
-instance Show m => Show (Pokemon m) where
-  show p = view (species . speciesName) p ++
-    " {" ++ "level = " ++ (show $ view pokemonLevel p) ++
-    ", ability = " ++ (show $ view possibleAbilities p) ++
-    ", item = " ++ (show $ view possibleHeldItem p) ++
-    ", moves = " ++ (show $ view moves p) ++
-    "}"
 
 -- Tilastojen laskeminen
 
@@ -78,16 +102,20 @@ getEv base p = view (pokemonEvs . l base) p
     l BaseSpeed = speedEv
 
 getBaseStat :: BaseStat -> Pokemon m -> Int
-getBaseStat base p = view (species . l base) p
+-- getBaseStat = undefined
+getBaseStat base = f . _species
   where
-    l BaseHP = baseHp
-    l BaseAttack = baseAttack
-    l BaseDefence = baseDefence
-    l BaseSAttack = baseSAttack
-    l BaseSDefence = baseSDefence
-    l BaseSpeed = baseSpeed
+    f = case base of
+      BaseHP -> baseHp
+      BaseAttack -> baseAttack
+      BaseDefence -> baseDefence
+      BaseSAttack -> baseSAttack
+      BaseSDefence -> baseSDefence
+      BaseSpeed -> baseSpeed
+
 
 newStat :: BaseStat -> Pokemon m -> Int
 newStat basestat pkmn =
   statisticCalc basestat (getBaseStat basestat pkmn) (view pokemonLevel pkmn) (view pokemonNature pkmn) (getIv basestat pkmn) $
   (getEv basestat pkmn)
+
