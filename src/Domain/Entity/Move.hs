9fd2f1fc553325ad99e084a1e7f80a4f0cf10cd0
Tommi Salenius <tommisalenius@gmail.com>
2021-02-25 00:52:20 +0200

    Eritelty damageliikkeet ja statusliikkeet omikseen

--- src/Domain/Entity/Move.hs
+++ src/Domain/Entity/Move.hs
@@ -1,217 +1,336 @@
-{-# LANGUAGE TemplateHaskell #-}
-
 module Domain.Entity.Move where
 
 import Domain.Attribute.Choice
 import Domain.Attribute.MoveExecution
 import Domain.Attribute.ModifStat
 import qualified Domain.Attribute.Damage as D
-import Domain.Match.Damage
-import Domain.Match.SideEffect
-import Domain.Match.BattleEffect
-import Domain.Match.MoveSucceeds
+import Domain.Match.SideEffect hiding (LightScreen, Reflect)
 import Domain.Match.Validation
 import qualified Domain.Match.Accuracy as Ac
-import Domain.Attribute.TypeOf
+import Domain.Attribute.TypeOf hiding (Psychic)
+import qualified Domain.Attribute.TypeOf as T
 import Domain.Attribute.Category
 import Domain.Attribute.Ailment
 import Domain.Attribute.Ability
 import qualified Domain.Attribute.Weather as W
 import Domain.Attribute.Counterparty
-import Control.Lens
-import Control.Lens.Prism
 
+data StatusMove =
+  AcidArmor
+  | LeechSeed
+  | LightScreen
+  | PainSplit
+  | QuiverDance
+  | Protect
+  | RainDance
+  | Reflect
+  | Rest
+  | Sandstorm
+  | SleepPowder
+  | SleepTalk
+  | SwordsDance
+  | Toxic
+  | WillOWisp
+  | Yawn
+  deriving (Eq,Show,Read,Ord,Enum)
 
-data Accuracy =
-  Accuracy Double
-  | AlwaysHits
-  deriving (Eq,Show,Ord)
+data DirectDamageMove =
+  MetalBurst
+  | SheerCold
+  deriving (Eq,Show,Read,Ord,Enum)
 
-defaultPowerPoints :: Int
-defaultPowerPoints = 15
-
-defaultChoiceProbability :: ChoiceRule Validation
-defaultChoiceProbability = ChoiceRule NormalProbability
-
-defaultSuccessionType :: SuccessType
-defaultSuccessionType = NormallyExecuted
-
-defaultHitProbability :: Ac.HitProbability
-defaultHitProbability = Ac.BasicAccuracy $ Ac.Accuracy 1.00
-
-data MoveSpecies = MoveSpecies
-  {
-    _nameOfMove :: String
-  , _typeOf :: TypeOf
-  , _category :: Category
-  , _powerPoints :: Int
-  , _priority :: Int
-  , _accuracy :: Accuracy
-  } deriving (Eq,Show)
-
-makeLenses ''MoveSpecies
-
-makeDefaultSpecies :: Category -> TypeOf -> MoveSpecies
-makeDefaultSpecies cat tp =
-  MoveSpecies "" tp cat defaultPowerPoints 0 $ Accuracy 1.0
-  
-
-data MoveNecessities = MoveNecessities
-  {
-    _species :: MoveSpecies
-  , _choiceProbability :: ChoiceRule Validation
-  , _executionSuccess :: SuccessType 
-  , _hitProbability :: Ac.HitProbability
-  , _sideEffect :: SideEffect
-  } deriving (Show)
-
-makeLenses ''MoveNecessities
-
-makeDefaultNecessities :: Category -> TypeOf -> SideEffect -> MoveNecessities
-makeDefaultNecessities cat tp eff =
-  MoveNecessities (makeDefaultSpecies cat tp) defaultChoiceProbability defaultSuccessionType defaultHitProbability eff
-
-data StrikeMove' = StrikeMove'
-  {
-  _basepower :: Maybe Int
-  , _makesContact :: Bool
-  , _damage :: Damage
-  , _strikeNecessities :: MoveNecessities
-  } deriving (Show)
-
-makeLenses ''StrikeMove'
-
-newtype StatusMove' = StatusMove' {_statusNecessities :: MoveNecessities}
-
-makeLenses ''StatusMove'
-
-data CallbackMove' m = CallbackMove'
-  {
-    _availableMoves :: [m]
-  , _callbackNecessities :: MoveNecessities
-  } deriving (Show)
-
-makeLenses ''CallbackMove'
+data DamageMove =
+  Struggle
+  | Acrobatics
+  | AirSlash
+  | AquaJet
+  | AuraSphere
+  | Avalanche
+  | Blizzard
+  | BodySlam
+  | BraveBird
+  | BugBuzz
+  | BulletPunch
+  | BrickBreak
+  | CloseCombat
+  | Crunch
+  | DarkPulse
+  | DracoMeteor
+  | DragonClaw
+  | DragonPulse
+  | EarthPower
+  | Earthquake
+  | EnergyBall
+  | ExtremeSpeed
+  | FakeOut
+  | FireBlast
+  | FirePunch
+  | Flamethrower
+  | FlareBlitz
+  | FlashCannon
+  | FocusBlast
+  | GigaDrain
+  | GrassKnot
+  | HammerArm
+  | HeadSmash
+  | HeatWave
+  | HydroPump
+  | IceBeam
+  | IceFang
+  | IcePunch
+  | IceShard
+  | IcyWind
+  | IronTail
+  | LeafStorm
+  | MachPunch
+  | MuddyWater
+  | Outrage
+  | Payback
+  | Psychic
+  | QuickAttack
+  | RockBlast
+  | RockSlide
+  | Scald
+  | SeedBomb
+  | ShadowBall
+  | SignalBeam
+  | SludgeBomb
+  | SolarBeam
+  | StoneEdge
+  | SuckerPunch
+  | Superpower
+  | Surf
+  | UTurn
+  | VoltTackle
+  | Waterfall
+  | WaterPulse
+  | WaterSpout
+  | WildCharge
+  | WoodHammer
+  | XScissor
+  | ZenHeadbutt
+  deriving (Eq,Show,Read,Ord,Enum)
 
 data Move =
-  StrikeMove StrikeMove'
-  | StatusMove StatusMove'
-  | CallbackMove (CallbackMove' Move)
+  DamageMove' DamageMove
+  | DirectDamageMove' DirectDamageMove
+  | StatusMove' StatusMove
+  deriving (Eq,Show,Read,Ord)
 
-makePrisms ''Move
+category' :: DamageMove -> Category'
+category' Acrobatics = Physical'
+category' AquaJet = Physical'
+category' Avalanche = Physical'
+category' BodySlam = Physical'
+category' BraveBird = Physical'
+category' BulletPunch = Physical'
+category' BrickBreak = Physical'
+category' CloseCombat = Physical'
+category' Crunch = Physical'
+category' DragonClaw = Physical'
+category' Earthquake = Physical'
+category' ExtremeSpeed = Physical'
+category' FakeOut = Physical'
+category' FirePunch = Physical'
+category' FlareBlitz = Physical'
+category' HammerArm = Physical'
+category' HeadSmash = Physical'
+category' IceFang = Physical'
+category' IcePunch = Physical'
+category' IceShard = Physical'
+category' IronTail = Physical'
+category' MachPunch = Physical'
+category' Outrage = Physical'
+category' Payback = Physical'
+category' QuickAttack = Physical'
+category' RockBlast = Physical'
+category' RockSlide = Physical'
+category' SeedBomb = Physical'
+category' StoneEdge = Physical'
+category' Struggle = Physical'
+category' SuckerPunch = Physical'
+category' Superpower = Physical'
+category' UTurn = Physical'
+category' VoltTackle = Physical'
+category' Waterfall = Physical'
+category' WildCharge = Physical'
+category' WoodHammer = Physical'
+category' XScissor = Physical'
+category' ZenHeadbutt = Physical'
+category' _ = Special'
 
-necessities :: Lens' Move MoveNecessities
-necessities = lens getter setter
-  where
-    getter m@(StrikeMove mv) = fromMaybe $ preview (_StrikeMove . strikeNecessities) m
-    getter m@(StatusMove mv) = fromMaybe $ preview (_StatusMove . statusNecessities) m
-    getter m@(CallbackMove mv) = fromMaybe $ preview (_CallbackMove . callbackNecessities) m
-    setter m@(StrikeMove mv) n = set (_StrikeMove . strikeNecessities) n m
-    setter m@(StatusMove mv) n = set (_StatusMove . statusNecessities) n m
-    setter m@(CallbackMove mv) n = set (_CallbackMove . callbackNecessities) n m
-    fromMaybe (Just a) = a
+category'' :: DirectDamageMove -> Category'
+category'' MetalBurst = Physical'
+category'' SheerCold = Special'
 
-instance Show Move where
-  show mv =
-    preview' (sp . nameOfMove) mv ++ " <" ++ typ mv ++ ", " ++ cat mv ++ ">"
+makesContact' :: DamageMove -> Bool
+makesContact' Earthquake = False
+makesContact' GrassKnot = True
+makesContact' RockBlast = False
+makesContact' StoneEdge = False
+makesContact' x
+  | category x == Physical = True
+  | otherwise = False
+
+makesContact'' :: DirectDamageMove -> Bool
+makesContact'' MetalBurst = False
+makesContact'' SheerCold = False
+
+instance TypeableMove StatusMove where
+  typeOfMove LeechSeed = Grass
+  typeOfMove LightScreen = T.Psychic
+  typeOfMove PainSplit = Normal
+  typeOfMove QuiverDance = Bug
+  typeOfMove Protect = Normal
+  typeOfMove RainDance = Water
+  typeOfMove Reflect = T.Psychic
+  typeOfMove Rest = T.Psychic
+  typeOfMove Sandstorm = Rock
+  typeOfMove SleepPowder = Grass
+  typeOfMove SleepTalk = Normal
+  typeOfMove SwordsDance = Normal
+  typeOfMove Toxic = Poison
+  typeOfMove WillOWisp = Fire
+  typeOfMove Yawn = Normal
+
+instance TypeableMove DamageMove where
+  typeOfMove Acrobatics = Flying
+  typeOfMove AquaJet = Water
+  typeOfMove Avalanche = Ice
+  typeOfMove AuraSphere = Fighting
+  typeOfMove Blizzard = Ice
+  typeOfMove BodySlam = Normal
+  typeOfMove BraveBird = Flying
+  typeOfMove BulletPunch = Steel
+  typeOfMove BrickBreak = Fighting
+  typeOfMove CloseCombat = Fighting
+  typeOfMove Crunch = Dark
+  typeOfMove DracoMeteor = Dragon 
+  typeOfMove EnergyBall = Grass
+  typeOfMove ExtremeSpeed = Normal
+  typeOfMove FakeOut = Normal
+  typeOfMove FlashCannon = Steel
+  typeOfMove FocusBlast = Fighting
+  typeOfMove GigaDrain = Grass
+  typeOfMove HammerArm = Fighting
+  typeOfMove HeadSmash = Rock
+  typeOfMove HeatWave = Fire
+  typeOfMove HydroPump = Water
+  typeOfMove IronTail = Steel
+  typeOfMove MachPunch = Fighting
+  typeOfMove MuddyWater = Water
+  typeOfMove Outrage = Dragon
+  typeOfMove Payback = Dark
+  typeOfMove QuickAttack = Normal
+  typeOfMove Scald = Water
+  typeOfMove SeedBomb = Grass
+  typeOfMove SignalBeam = Bug
+  typeOfMove SludgeBomb = Poison
+  typeOfMove SolarBeam = Grass
+  typeOfMove Struggle = Normal
+  typeOfMove SuckerPunch = Dark
+  typeOfMove Superpower = Fighting
+  typeOfMove Surf = Water
+  typeOfMove UTurn = Bug
+  typeOfMove WildCharge = Electric
+  typeOfMove WoodHammer = Grass
+  typeOfMove XScissor = Bug
+  typeOfMove ZenHeadbutt = T.Psychic
+  typeOfMove x
+    | take' 5 == "Water" = Water
+    | take' 4 == "Rain" = Water
+    | take' 4 == "Fire" = Fire
+    | take' 5 == "Flame" = Fire
+    | take' 5 == "Flare" = Fire
+    | take' 6 == "Thunder" = Electric
+    | take' 4 == "Volt" = Electric
+    | take' 5 == "Grass" = Grass
+    | take' 4 == "Leaf" = Grass
+    | take' 3 == "Ice" = Ice
+    | take' 3 == "Icy" = Ice
+    | take' 5 == "Earth" = Ground
+    | take' 4 == "Rock" = Rock
+    | take' 5 == "Stone" = Rock
+    | take' 6 == "Poison" = Poison
+    | take' 6 == "Shadow" = Ghost
+    | take' 3 == "Bug" = Bug
+    | take' 6 == "Dragon" = Dragon
+    | take' 4 == "Dark" = Dark
+    | take' 5 == "Metal" = Steel
+    | take' 3 == "Air" = Flying
+    | take' 7 == "Psychic" = T.Psychic
     where
-      preview' a b = fromMaybe . preview a $ b
-      sp = necessities . species
-      cat m = show . fromMaybe $ preview (necessities . species . category) m
-      typ m = show . fromMaybe $ preview (necessities . species . typeOf) m
-      fromMaybe (Just a) = a
+      take' n = take n . show $ x
 
-instance Eq Move where
-  mv == mv' =
-    preview l mv == preview l mv' ||
-    preview l' mv == preview l' mv' ||
-    preview l'' mv == preview l'' mv' 
-    where
-      l = _StrikeMove . strikeNecessities . species
-      l' = _StatusMove . statusNecessities . species
-      l'' = _CallbackMove . callbackNecessities . species
+instance TypeableMove DirectDamageMove where
+  typeOfMove MetalBurst = Steel
+  typeOfMove SheerCold = Ice
 
-instance Ord Move where
-  StrikeMove m <= StatusMove m' = False
-  StrikeMove m <= StrikeMove m' =
-    (preview basepower $ m) <= (preview basepower $ m')
-  _ <= _ = False
+instance TypeableMove Move where
+  typeOfMove (DamageMove' mv) = typeOfMove mv
+  typeOfMove (StatusMove' mv) = typeOfMove mv
+  typeOfMove (DirectDamageMove' mv) = typeOfMove mv
 
-makeStatusMove :: TypeOf -> SideEffect -> Move
-makeStatusMove tp eff =
-  StatusMove . StatusMove' $ makeDefaultNecessities Status tp eff
-    
+basepower :: DamageMove -> Maybe Int
+basepower Acrobatics = Just 55
+basepower AquaJet = Just 40
+basepower BrickBreak = Just 75
+basepower DracoMeteor = Just 130
+basepower FakeOut = Just 40
+basepower FireBlast = Just 110
+basepower GrassKnot = Nothing
+basepower IronTail = Just 100
+basepower LeafStorm = Just 130
+basepower ShadowBall = Just 80
+basepower Struggle = Just 50
+basepower UTurn = Just 70
+basepower VoltTackle = Just 120
+basepower WaterSpout = Just 150
 
-makeStrikeMove :: Category -> Int -> TypeOf -> SideEffect -> Move
-makeStrikeMove cat basep tp eff =
-  StrikeMove $ StrikeMove'
-  {
-    _basepower = Just basep
-  , _makesContact = if cat == Physical then True else False
-  , _damage =
-    set (_StandardDamage . stab) (Stab tp 1.5) $
-    set (_StandardDamage . typeAdvantage) (TypeAdvantage tp) $
-    defaultDamage basep cat 
-  , _strikeNecessities = makeDefaultNecessities cat tp eff
-  
-  }
+-- Apumäärittely
 
-makeCallbackMove :: TypeOf -> [Move] -> Move
-makeCallbackMove tp mvs =
-  CallbackMove $ CallbackMove'
-  {
-    _availableMoves = mvs
-  , _callbackNecessities = makeDefaultNecessities Status tp mempty
-  }
+data Category' = Special' | Physical' deriving (Eq,Show,Read,Ord,Enum)
 
-physicalMove = makeStrikeMove Physical
-specialMove = makeStrikeMove Special
+class CategorizableMove m where
+  category :: m -> Category
 
--- noContact x = x { _makesContact = False}
-noContact :: Move -> Move
-noContact  = set (_StrikeMove . makesContact) False
+class WrappableMove m where
+  wrapMove :: m -> Move
 
-assignName :: String -> Move -> Move
-assignName nm = set (necessities . species . nameOfMove) nm
+class TypeableMove m where
+  typeOfMove :: m -> TypeOf
 
-assignPP :: Int -> Move -> Move
-assignPP pp = set (necessities . species . powerPoints) pp
+instance CategorizableMove StatusMove where
+  category _ = Status
 
-assignAccuracy :: Double -> Move -> Move
-assignAccuracy prob =
-  set (necessities . species . accuracy) (Accuracy prob) .
-  set (necessities . hitProbability) (Ac.BasicAccuracy $ Ac.Accuracy prob)
+instance CategorizableMove DamageMove where
+  category mv = case category' mv of
+    Special'  -> Special
+    Physical' -> Physical
 
-assignChoiceProbability :: ChoiceRule Validation -> Move -> Move
-assignChoiceProbability cr = set (necessities . choiceProbability) cr
-
-alwaysHits :: Move -> Move
-alwaysHits = set (necessities . species . accuracy) AlwaysHits
-
-assignPriority :: Int -> Move -> Move
-assignPriority prio = set (necessities . species . priority) prio
-
-effects :: SideEffect -> Move -> Move
-effects es = set (necessities . sideEffect) es
+instance CategorizableMove DirectDamageMove where
+  category mv = case category'' mv of
+    Special'  -> Special
+    Physical' -> Physical
 
 
-modifyBaseDamage :: Int -> ModificationSource -> (Int -> Int) -> Move -> Move
-modifyBaseDamage b src fn = 
-  set (_StrikeMove . damage . _StandardDamage . baseDamage . _ModifiedBaseDamage) x
-  where
-    x = ModifiedBaseDamage' b fn src
+instance WrappableMove DamageMove where
+  wrapMove = DamageMove'
 
-modifyOtherFactors :: (OtherFactors -> OtherFactors) -> Move -> Move
-modifyOtherFactors factor m = 
-  over (_StrikeMove . damage . _StandardDamage . otherFactors) (\o -> factor o) m 
+instance WrappableMove StatusMove where
+  wrapMove = StatusMove'
 
-modifyCriticalHit :: (CriticalHitCalc -> CriticalHitCalc) -> Move -> Move
-modifyCriticalHit crhit = 
-  over (_StrikeMove . damage . _StandardDamage . criticalHitCalc) (\o -> crhit o)
+instance WrappableMove DirectDamageMove where
+  wrapMove = DirectDamageMove'
 
-targetAbilityImmunity :: Ability -> Validation
-targetAbilityImmunity ab = notMoldBreaker `Or` (abilityIs Target $ (==) ab)
+allAttackingMoves :: [DamageMove]
+allAttackingMoves = enumFrom Acrobatics
 
-bulletproof = targetAbilityImmunity Bulletproof
-soundproof = targetAbilityImmunity Soundproof
+allStatusMoves :: [StatusMove]
+allStatusMoves = enumFrom AcidArmor
 
+allDirectDamageMoves :: [DirectDamageMove]
+allDirectDamageMoves = enumFrom MetalBurst
+
+allMoves :: [Move]
+allMoves = (wrapMove <$> allStatusMoves) ++ (wrapMove <$> allAttackingMoves) ++ (wrapMove <$> allDirectDamageMoves)
