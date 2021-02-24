442f24e94182cba9e9f8953925559a423493a581
Tommi Salenius <tommisalenius@gmail.com>
2021-02-25 00:55:03 +0200

    Uudistettu varovaisesti hajottamalla isoja algebroja pienemmiksi

--- src/Domain/Battle/Algebra.hs
+++ src/Domain/Battle/Algebra.hs
@@ -7,103 +7,25 @@ import Domain.Attribute.MoveExecution
 import Domain.Attribute.Weather as W
 import Domain.Entity.Battle as B
 import Domain.Entity.PokemonState
+import Domain.Entity.PlayerState
 import Control.Monad.State
 import Control.Lens
 import Data.Maybe
 
-class BattleAlgebra b where
-  getUser :: b -> Maybe Player
-  getCurrentPokemon :: Player -> b -> Maybe PokemonState
-  -- getNextPokemon :: Player -> b -> Maybe PokemonState
-  updateCurrentPokemon :: (PokemonState -> PokemonState) -> Player -> b -> b
-  -- updatePlayer :: (PlayerState -> PlayerState) -> Player -> b -> b
-  getTarget :: b -> Maybe Player
-  getTarget battle
-    | getUser battle == Just Player1 = Just Player2
-    | getUser battle == Just Player2 = Just Player1
-    | otherwise = Nothing
-  getWeather :: b -> Maybe W.Weather
-  getPlayer :: Player -> b -> Maybe PlayerState
-  updatePlayer :: (PlayerState -> PlayerState) -> Player -> b -> b
-  setWeather :: W.Weather -> Int -> b -> b
+getCurrentPokemon :: PlayerState -> PokemonState
+getCurrentPokemon = view currentPokemon
 
-class ReadPreviousStrike b where
-  askPrevStrike :: b -> Maybe MoveExecution
+class BattleView b where
+  getBattle :: b -> BattleState
 
-instance BattleAlgebra Battle where
-  getUser = preview user
-  getCurrentPokemon plr battle = preview (player plr . currentPokemon . _AbleToBattle) battle
-  updateCurrentPokemon f plr battle = over (player plr . currentPokemon . _AbleToBattle) f battle
-  getWeather = preview (_OnGoingBattle . battleState . weather . currentWeatherState)
-  getPlayer pl = preview (player pl)
-  updatePlayer f pl = over (player pl) f
-  setWeather W.Normal _ = set (_OnGoingBattle . battleState . B.weather) (PermanentWeather W.Normal)
-  setWeather w x = set (_OnGoingBattle . battleState . B.weather) (TemporaryWeather $ TemporaryWeather' w x)
+instance BattleView BattleState where
+  getBattle = id
 
--- Battle-datatyypi ei itsessään sisällä tietoja viimeisestä liikkeestä
-instance ReadPreviousStrike Battle where
-  askPrevStrike _ = Nothing
+getPlayer :: BattleView b => Player -> b -> PlayerState
+getPlayer plr = fn plr . getBattle
+  where
+    fn Player1 = view player1
+    fn Player2 = view player2
 
-attributeSatisfies :: (Eq a, BattleAlgebra b) => (b -> Maybe a) -> (a -> Bool) -> (b -> Bool)
-attributeSatisfies getter cond battle = case getter battle >>= return . cond of
-  Just x -> x
-  Nothing -> False
-
-getUserPokemon f battle = getUser battle >>= flip getCurrentPokemon battle >>= return . f
-getTargetPokemon f battle = getTarget battle >>= flip getCurrentPokemon battle >>= return . f
-
-modifyCpPokemon ::
-  BattleAlgebra b
-  => (b -> Maybe Player)
-  -> (PokemonState -> PokemonState)
-  -> b
-  -> b
-modifyCpPokemon getter f battle = fromMaybe battle $ do
-  usr <- getter battle
-  let battle' = updateCurrentPokemon f usr battle
-  return battle'
-
-modifyUserPokemon ::
-  BattleAlgebra b
-  => (PokemonState -> PokemonState)
-  -> b
-  -> b
-modifyUserPokemon = modifyCpPokemon getUser
-
-modifyTargetPokemon ::
-  BattleAlgebra b
-  => (PokemonState -> PokemonState)
-  -> b
-  -> b
-modifyTargetPokemon = modifyCpPokemon getTarget
-
-modifyCp ::
-  BattleAlgebra b
-  => (b -> Maybe Player)
-  -> (PlayerState -> PlayerState)
-  -> b
-  -> b
-modifyCp getter f battle = fromMaybe battle $ do
-  usr <- getter battle
-  let battle' = updatePlayer f usr battle
-  return battle'
-
-modifyUser ::
-  BattleAlgebra b
-  => (PlayerState -> PlayerState)
-  -> b
-  -> b
-modifyUser = modifyCp getUser
-
-modifyTarget ::
-  BattleAlgebra b
-  => (PlayerState -> PlayerState)
-  -> b
-  -> b
-modifyTarget = modifyCp getTarget
-
-updateBattle :: (MonadState b m, BattleAlgebra b) => (b -> b) -> m ()
-updateBattle fn = do
-  battle <- get
-  let battle' = fn battle
-  put battle'
+getPokemon :: BattleView b => Player -> b -> PokemonState
+getPokemon plr = getCurrentPokemon . getPlayer plr
