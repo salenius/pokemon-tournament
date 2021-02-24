b7eb5eec921983aeede36cc0230c2a048c2840da
Tommi Salenius <tommisalenius@gmail.com>
2021-02-25 00:47:11 +0200

    GenderRatiosta tehty datatyyppinsä

--- src/Domain/Attribute/Gender.hs
+++ src/Domain/Attribute/Gender.hs
@@ -5,3 +5,12 @@ data Gender =
   | Female
   | Genderless
   deriving (Eq,Show,Read)
+
+data GenderRatio =
+  Male7to1
+  | Male3to1
+  | EvenRatio
+  | AlwaysGenderless
+  | AlwaysMale
+  | AlwaysFemale
+  deriving (Eq,Show,Read)
