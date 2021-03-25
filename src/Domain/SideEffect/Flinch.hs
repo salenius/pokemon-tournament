ea2c52d86ff18d0ab6c5e1e12b94dc4731a3765f
Tommi Salenius <tommisalenius@gmail.com>
2021-03-25 12:13:48 +0200

    Luotu logiikkaa flinchille

---
+++ src/Domain/SideEffect/Flinch.hs
@@ -0,0 +1,56 @@
+{-# LANGUAGE RecordWildCards #-}
+
+module Domain.SideEffect.Flinch where
+
+import Domain.Attribute.Ability
+import Domain.Attribute.Counterparty
+import Data.Maybe
+import Control.Applicative
+
+data FlinchCondition sideEff =
+  GeneralCondition
+  | TargetHasSteadfast sideEff
+  | TargetHasInnerFocus
+  | UserHasMoldBreaker IsMoldBreaker (FlinchCondition sideEff)
+  deriving (Eq,Show)
+
+instance Functor FlinchCondition where
+  fmap f (TargetHasSteadfast x) = TargetHasSteadfast $ f x
+  fmap f (UserHasMoldBreaker m x) = UserHasMoldBreaker m $ fmap f x
+  fmap _ GeneralCondition = GeneralCondition
+  fmap _ TargetHasInnerFocus = TargetHasInnerFocus
+
+data FlinchData battle = FlinchData
+  {
+    abilityOfPokemon :: Counterparty -> battle -> Ability
+  }
+
+parseFlinch :: FlinchData battle -> battle -> FlinchCondition ()
+parseFlinch FlinchData{..} battle =
+  fromMaybe GeneralCondition $
+  parseSteadfast abilityOfPokemon battle <|>
+  parseMoldBreaker abilityOfPokemon battle <|>
+  parseInnerFocus abilityOfPokemon battle 
+
+
+------
+
+parseSteadfast :: (Counterparty -> b -> Ability) -> b -> Maybe (FlinchCondition ())
+parseSteadfast fn b =
+  case fn Target b of
+    Steadfast -> Just (TargetHasSteadfast ())
+    _ -> Nothing
+
+
+parseInnerFocus :: (Counterparty -> b -> Ability) -> b -> Maybe (FlinchCondition ())
+parseInnerFocus fn b =
+  case fn Target b of
+    InnerFocus -> Just TargetHasInnerFocus
+    _ -> Nothing
+
+
+parseMoldBreaker :: (Counterparty -> b -> Ability) -> b -> Maybe (FlinchCondition ())
+parseMoldBreaker fn b = do
+  inf' <- parseInnerFocus fn b
+  mbr' <- isMoldBreaker $ fn User b
+  return $ UserHasMoldBreaker mbr' inf'
