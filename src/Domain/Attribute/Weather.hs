382750d4923e6c93d3fa8e53dff941cd6bcd9244
Tommi Salenius <tommisalenius@gmail.com>
2021-04-27 23:14:40 +0300

    Luotu rajapinta sään keston vaikutuksen säätämiselle
    WeatherStatus-datatyyppi hoitaa tämän

--- src/Domain/Attribute/Weather.hs
+++ src/Domain/Attribute/Weather.hs
@@ -8,6 +8,24 @@ data Weather =
   | Sandstorm
   deriving (Show,Eq)
 
+data WeatherStatus =
+  NormalWeather
+  | SunnyWeather Rounds
+  | RainyWeather Rounds
+  | HailWeather Rounds
+  | SandstormWeather Rounds
+  deriving (Eq,Show)
+
+type Rounds = Int
+
+weatherStatusToWeather :: WeatherStatus -> Weather
+weatherStatusToWeather ws = case ws of
+  NormalWeather -> Normal
+  SunnyWeather _ -> Sunny
+  RainyWeather _ -> Rainy
+  HailWeather _ -> Hail
+  SandstormWeather _ -> Sandstorm
+
 allWeathers :: [Weather]
 allWeathers =
   [
