29012885068ef475a3ed51f0b2cfd8d457cbfa16
Tommi Salenius <tommisalenius@gmail.com>
2021-05-01 15:11:33 +0300

    Luotu uudet type classit Pokemonien ja liikkeiden tulkitsemiseksi
    - AsMoveOp
    - AsPokemonspecies
    - FromTrainer
    
    ja lisätty näille instanssit

--- src/Domain/Battle/Algebra.hs
+++ src/Domain/Battle/Algebra.hs
@@ -1,31 +1,67 @@
-{-# LANGUAGE FlexibleContexts, GADTs #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ViewPatterns #-}
+{-# LANGUAGE FlexibleInstances #-}
+
 
 module Domain.Battle.Algebra where
 
-import Domain.Attribute.Player
-import Domain.Attribute.MoveExecution
-import Domain.Attribute.Weather as W
-import Domain.Entity.Battle as B
-import Domain.Entity.PokemonState
-import Domain.Entity.PlayerState
-import Control.Monad.State
 import Control.Lens
-import Data.Maybe
+import Data.List.NonEmpty
+import Domain.Attribute.Quadruple
+import Domain.Battle.Op
+import Domain.Battle.State
+import Domain.Battle.State.Pokemon
+import Domain.Battle.Effect
+import Domain.Entity.BuiltIn.Move
+import Domain.Entity.Stats.Pokemon
+import Domain.Entity.BuiltIn.Pokemon
+import Domain.Entity.Pokemon
+import qualified Domain.Entity.BuiltIn.Move.Status as Status
+import qualified Domain.Entity.BuiltIn.Move.Attacking as Attacking
 
-getCurrentPokemon :: PlayerState -> PokemonState
-getCurrentPokemon = view currentPokemon
+class AsMoveOp mv where
+  toMoveOp :: mv -> MoveOp BattleEffect BattleState 
 
-class BattleView b where
-  getBattle :: b -> BattleState
+class AsPokemonSpecies pkmn where
+  toPokemonSpecies :: pkmn -> (PokemonSpecies, Quadruple (MoveOp BattleEffect BattleState))
 
-instance BattleView BattleState where
-  getBattle = id
+class FromTrainer trnr where
+  fromTrainer :: trnr -> NonEmpty (PokemonSpecies, Quadruple (MoveOp BattleEffect BattleState))
 
-getPlayer :: BattleView b => Player -> b -> PlayerState
-getPlayer plr = fn plr . getBattle
-  where
-    fn Player1 = view player1
-    fn Player2 = view player2
 
-getPokemon :: BattleView b => Player -> b -> PokemonState
-getPokemon plr = getCurrentPokemon . getPlayer plr
+---
+
+-- Nämä pitää korjata niin, että eri liikkeiden vaikutukset implementoidaan kunnolla
+
+instance AsMoveOp Status.Move where
+  toMoveOp _ = MoveOp return $ randomInt (1,10)
+
+instance AsMoveOp Attacking.Move where
+  toMoveOp _ = MoveOp return $ randomInt (1,10)
+
+instance AsMoveOp Move where
+  toMoveOp (preview _StatusMove -> Just mv) = toMoveOp mv
+  toMoveOp (preview _DamagingMove -> Just mv) = toMoveOp mv
+
+instance AsPokemonSpecies (Pokemon Red'sPokemon) where
+  toPokemonSpecies = pokemonTuple
+
+instance AsPokemonSpecies (Pokemon Blue'sPokemon) where
+  toPokemonSpecies = pokemonTuple
+
+instance AsPokemonSpecies (Pokemon Lance'sPokemon) where
+  toPokemonSpecies = pokemonTuple
+
+instance AsPokemonSpecies (Pokemon Steven'sPokemon) where
+  toPokemonSpecies = pokemonTuple
+
+instance AsPokemonSpecies (Pokemon Wallace'sPokemon) where
+  toPokemonSpecies = pokemonTuple
+
+instance AsPokemonSpecies (Pokemon Cynthia'sPokemon) where
+  toPokemonSpecies = pokemonTuple
+
+instance AsPokemonSpecies (Pokemon Alder'sPokemon) where
+  toPokemonSpecies = pokemonTuple
+
+pokemonTuple pkmn = (mkPokemonSpecies pkmn, fmap toMoveOp . moves . view pokemonSpecies $ pkmn)
