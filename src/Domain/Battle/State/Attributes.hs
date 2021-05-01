4fe62da44f761d4728fd00b1f5dc5fe0cd5be09b
Tommi Salenius <tommisalenius@gmail.com>
2021-05-01 15:10:54 +0300

    Luotu polymorfinen PlayersInBattle-datatyyppi

--- src/Domain/Battle/State/Attributes.hs
+++ src/Domain/Battle/State/Attributes.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE TemplateHaskell, FlexibleInstances #-}
+
 module Domain.Battle.State.Attributes where
 
 import Domain.Attribute.Player
@@ -7,23 +9,36 @@ import Domain.Attribute.Screen
 import Domain.Attribute.HeldItem
 import Domain.Attribute.TypeOf
 import Domain.Attribute.ModifStat
-import Domain.Battle.Algebra
+import Domain.Battle.Id
+import Domain.Battle.State.Pokemon
 import Domain.Attribute.Quadruple
 import Domain.Attribute.Ability
 import Data.Map (Map, fromList, lookup)
+import Control.Lens
 
-data BattleAttributes = BattleAttributes
+data PlayersInBattle plr = PlayersInBattle
   {
-    _player1 :: PlayerState
-  , _player2 :: PlayerState
-  }
+    _player1 :: plr
+  , _player2 :: plr
+  } deriving (Eq,Show)
 
-data PlayerState = PlayerState
+data PlayerInBattle =
+  StillFighting (PlayerState PokemonInBattle)
+  | Defeated
+  deriving (Eq,Show)
+
+data CounterpartiesInStrike = CounterpartiesInStrike
   {
-    _currentPokemonInUse :: PokemonState
-  , _restOfPokemonParty :: [PokemonState]
-  , _playerWideAttributes :: PlayerAttributes
-  }
+    _currentUser :: PlayerInBattle
+  , _currentTarget :: PlayerInBattle
+  } deriving (Eq,Show)
+
+data PlayerState pkmn = PlayerState
+  {
+    _currentPokemonInUse :: pkmn
+  , _restOfPokemonParty :: [PokemonInBattle]
+  , _playerAttributesAll :: PlayerAttributes
+  } deriving (Eq,Show)
 
 data PlayerAttributes = PlayerAttributes
   {
@@ -32,29 +47,32 @@ data PlayerAttributes = PlayerAttributes
   , _playerSafeguard :: Safeguard
   } deriving (Eq,Show)
 
+data PokemonInBattle =
+  AlivePokemon PokemonState
+  | UnableToBattle
+  deriving (Eq,Show)
+
 data PokemonState = PokemonState
   {
     _pokemonSpecies :: PokemonSpecies
-  , _pokemonHp :: HP
-  , _pokemonAttributes :: PokemonAttributes
-  , _pokemonStatus :: PokemonStatus
-  }
+  , _pokemonCurrentHp :: HP
+  , _pokemonAttributesAll :: PokemonAttributes
+  , _pokemonStatusAll :: PokemonStatus
+  } deriving (Eq,Show)
 
 data PokemonAttributes = PokemonAttributes
   {
     _pokemonTempTypeOf :: Maybe [TypeOf]
   , _pokemonTempAbility :: Maybe Ability
   , _pokemonMoves :: Quadruple MoveState
-  , _pokemonMoveLocked :: Maybe MoveId
-  , _pokemonMoveCharging :: Maybe MoveId
-  }
+  } deriving (Eq,Show)
 
 data MoveState = MoveState
   {
     _moveId :: MoveId
   , _ppLeft :: Int
   , _moveTempTypeOf :: Maybe TypeOf
-  }
+  } deriving (Eq,Show)
 
 data PokemonStatus = PokemonStatus
   {
@@ -64,4 +82,49 @@ data PokemonStatus = PokemonStatus
   , _pokemonProtected :: Protected
   , _pokemonLeechSeeded :: LeechSeeded
   , _pokemonYawned :: Yawned
-  }
+  , _pokemonMoveLocked :: Maybe MoveId
+  , _pokemonMoveCharging :: Maybe MoveId
+  } deriving (Eq,Show)
+
+makeClassy ''PokemonStatus
+makeClassy ''MoveState
+makeClassy ''PokemonAttributes
+makeClassy ''PokemonState
+makeLenses ''PlayerAttributes
+makeLenses ''PlayerState
+
+instance Functor PlayerState where
+  fmap f plr = plr {_currentPokemonInUse = f $ _currentPokemonInUse plr}
+
+instance Functor PlayersInBattle where
+  fmap f btl = btl {_player1 = f $ _player1 btl, _player2 = f $ _player2 btl}
+
+instance Semigroup m => Semigroup (PlayerState m) where
+  plr1 <> plr2 =
+    over currentPokemonInUse ((<>) (view currentPokemonInUse plr1)) .
+    over restOfPokemonParty ((<>) (view restOfPokemonParty plr1)) .
+    over playerAttributesAll ((<>) (view playerAttributesAll plr1)) $
+    plr2
+
+instance Monoid m => Monoid (PlayerState m) where
+  mempty = PlayerState mempty mempty mempty
+
+instance Semigroup PlayerAttributes where
+  plr1 <> plr2 =
+    over playerLightScreen ((<>) (view playerLightScreen plr1)) .
+    over playerReflect ((<>) (view playerReflect plr1)) .
+    over playerSafeguard ((<>) (view playerSafeguard plr1)) $
+    plr2
+
+instance Monoid PlayerAttributes where
+  mempty = PlayerAttributes mempty mempty mempty
+
+instance HasPokemonStatus PokemonState where
+  pokemonStatus = pokemonStatusAll
+
+instance HasPokemonAttributes PokemonState where
+  pokemonAttributes = pokemonAttributesAll
+
+instance HasPokemonState (PlayerState PokemonState) where
+  pokemonState = currentPokemonInUse
+
