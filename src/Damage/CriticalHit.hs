14d23a41518a5c226617010b2c015a25bae769e8
Tommi Salenius <tommisalenius@gmail.com>
2021-05-14 11:48:34 +0300

    Kriittisen iskun laskentaa paranneltu
    - lisätty Battle / ShellArmor, ScopeLens, Merciless

--- src/Damage/CriticalHit.hs
+++ src/Damage/CriticalHit.hs
@@ -9,6 +9,12 @@ module Damage.CriticalHit where
 
 import Damage.Interpreter
 import Control.Lens
+import Control.Applicative
+import Attribute.Ability
+import Attribute.Ailment
+import Attribute.Item
+import Attribute.Counterparty
+import Helper.Parse
 
 data CriticalHit = NotCritical | IsCritical deriving (Eq,Show,Ord,Enum)
 
@@ -34,18 +40,97 @@ data CriticalHitCalc a m = CriticalHitCalc
   {
     _initialLevel :: CriticalHitLevel
   , _levelIntToProb :: Int -> Prob
+  , _manipulateProb :: Prob -> m Prob
   , _drawHit :: a -> Prob -> m Double
   }
 
 makeLenses ''CriticalHitCalc
 
+mkCriticalHitCalc
+  :: DamageOps m
+  => (a -> Counterparty -> PokemonAbility)
+  -> (a -> Counterparty -> Ailment)
+  -> (a -> Counterparty -> HeldItem)
+  -> a
+  -> CriticalHitCalc a m
+mkCriticalHitCalc abfn ailfn itfn dta =
+  CriticalHitCalc lvl genVCriticalHit f $ \_ _ -> drawRandomDouble
+  where
+    f pr = return $ battleArmor abfn dta $ merciless abfn ailfn dta $ pr
+    lvl = scopeLens itfn dta $ Level0
+
 reduce :: DamageOps m => CriticalHitCalc a m -> a -> m CriticalHit
 reduce calc dta = do
   v <- drawRandomDouble
   let lvl = view levelIntToProb calc . levelToInt $ view initialLevel calc
-  h <- view drawHit calc dta lvl
+  lvl' <- view manipulateProb calc lvl
+  h <- view drawHit calc dta lvl'
   let res = case (v < h) of
               True  -> IsCritical
               False -> NotCritical
   return res
     
+
+battleArmor :: (a -> Counterparty -> PokemonAbility) -> a -> Prob -> Prob
+battleArmor abfn dta prb =
+  parseAndAct' m' f dta prb
+  where
+    m prsm cnstr x = do
+      let usrAb = abfn x User
+      let trgtAb = abfn x Target
+      shllArmr <- preview prsm trgtAb
+      notMb <- notMoldBreaker usrAb
+      return $ BattleArmorData (cnstr shllArmr) notMb
+    m' x = (m _BattleArmor Left x) <|> (m _ShellArmor Right x)
+    f _ _ = 0
+
+merciless ::
+  (a -> Counterparty -> PokemonAbility) -> (a -> Counterparty -> Ailment) -> a -> Prob -> Prob
+merciless abfn ailfn dta prb =
+  parseAndAct' m f dta prb
+  where
+    m x = do
+      let usrAb = abfn x User
+      let trgtAi = ailfn x Target
+      mrclss <- preview _Merciless usrAb
+      psnd <- preview _Poisoned trgtAi
+      return (mrclss, psnd)
+    f _ _ = 1
+
+scopeLens :: (a -> Counterparty -> HeldItem) -> a -> CriticalHitLevel -> CriticalHitLevel
+scopeLens itemFn dta lvl =
+  parseAndAct' m f dta lvl
+  where
+    m x = do
+      let item = itemFn dta User
+      sl <- preview _ScopeLens item
+      return $ sl
+    f _ = increaseLevel
+
+data BattleArmorData = BattleArmorData (Either BattleArmor ShellArmor) ()
+
+increaseLevel :: CriticalHitLevel -> CriticalHitLevel
+increaseLevel Level4 = Level4
+increaseLevel x = succ x
+
+genVCriticalHit :: Int -> Prob
+genVCriticalHit = \case
+  0 -> 0.0625
+  1 -> 0.125
+  2 -> 0.25
+  3 -> 0.333
+  _ -> 0.5
+
+genVICriticalHit :: Int -> Prob
+genVICriticalHit = \case
+  0 -> 0.0625
+  1 -> 0.125
+  2 -> 0.50
+  _ -> 1.00
+
+genVIICriticalHit :: Int -> Prob
+genVIICriticalHit = \case
+  0 -> 0.04167
+  1 -> 0.125
+  2 -> 0.5
+  _ -> 1.00
