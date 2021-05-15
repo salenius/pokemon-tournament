bc6ba05ade4501192916d82e7af5736dfa0e78c7
Tommi Salenius <tommisalenius@gmail.com>
2021-05-15 11:49:51 +0300

    Lisätty prismat tyypeille

--- src/Attribute/TypeOf.hs
+++ src/Attribute/TypeOf.hs
@@ -8,6 +8,7 @@ module Attribute.TypeOf where
 import Types.Pokemon
 import Types.BuiltIn
 import GHC.Generics
+import Control.Lens
 
 class IsTypeOf o where
   asNormal :: o -> Maybe NormalType
@@ -68,3 +69,58 @@ instance IsTypeOf PokemonType where
   asGhost pt =  if Ghost `elem` pt then Just GhostType else Nothing
   asDragon pt =  if Dragon `elem` pt then Just DragonType else Nothing
   asFairy pt =  if Fairy `elem` pt then Just FairyType else Nothing
+
+_NormalType :: Prism' TypeOf NormalType
+_NormalType = prism' (\_ -> Normal) (\x -> case x of Normal -> Just NormalType; _ -> Nothing)
+
+_FightingType :: Prism' TypeOf FightingType
+_FightingType = prism' (\_ -> Fighting) (\x  -> case x of Fighting -> Just FightingType; _ -> Nothing)
+
+_FlyingType :: Prism' TypeOf FlyingType
+_FlyingType = prism' (\_ -> Flying) (\x  -> case x of Flying -> Just FlyingType; _ -> Nothing)
+
+_WaterType :: Prism' TypeOf WaterType
+_WaterType = prism' (\_ -> Water) (\x  -> case x of Water -> Just WaterType; _ -> Nothing)
+
+_FireType :: Prism' TypeOf FireType
+_FireType = prism' (\_ -> Fire) (\x  -> case x of Fire -> Just FireType; _ -> Nothing)
+
+_GrassType :: Prism' TypeOf GrassType
+_GrassType = prism' (\_ -> Grass) (\x  -> case x of Grass -> Just GrassType; _ -> Nothing)
+
+_ElectricType :: Prism' TypeOf ElectricType
+_ElectricType = prism' (\_ -> Electric) (\x  -> case x of Electric -> Just ElectricType; _ -> Nothing)
+
+_BugType :: Prism' TypeOf BugType
+_BugType = prism' (\_ -> Bug) (\x  -> case x of Bug -> Just BugType; _ -> Nothing)
+
+_PoisonType :: Prism' TypeOf PoisonType
+_PoisonType = prism' (\_ -> Poison) (\x  -> case x of Poison -> Just PoisonType; _ -> Nothing)
+
+_RockType :: Prism' TypeOf RockType
+_RockType = prism' (\_ -> Rock) (\x  -> case x of Rock -> Just RockType; _ -> Nothing)
+
+_GroundType :: Prism' TypeOf GroundType
+_GroundType = prism' (\_ -> Ground) (\x  -> case x of Ground -> Just GroundType; _ -> Nothing)
+
+_SteelType :: Prism' TypeOf SteelType
+_SteelType = prism' (\_ -> Steel) (\x  -> case x of Steel -> Just SteelType; _ -> Nothing)
+
+_IceType :: Prism' TypeOf IceType
+_IceType = prism' (\_ -> Ice) (\x  -> case x of Ice -> Just IceType; _ -> Nothing)
+
+_PsychicType :: Prism' TypeOf PsychicType
+_PsychicType = prism' (\_ -> Psychic) (\x  -> case x of Psychic -> Just PsychicType; _ -> Nothing)
+
+_DarkType :: Prism' TypeOf DarkType
+_DarkType = prism' (\_ -> Dark) (\x  -> case x of Dark -> Just DarkType; _ -> Nothing)
+
+_GhostType :: Prism' TypeOf GhostType
+_GhostType = prism' (\_ -> Ghost) (\x  -> case x of Ghost -> Just GhostType; _ -> Nothing)
+
+_DragonType :: Prism' TypeOf DragonType
+_DragonType = prism' (\_ -> Dragon) (\x  -> case x of Dragon -> Just DragonType; _ -> Nothing)
+
+_FairyType :: Prism' TypeOf FairyType
+_FairyType = prism' (\_ -> Fairy) (\x  -> case x of Fairy -> Just FairyType; _ -> Nothing)
+
