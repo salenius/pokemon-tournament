aa4f4cb817df34c34ccb1f6d06c020363c3c5637
Tommi Salenius <tommisalenius@gmail.com>
2021-06-11 12:52:10 +0300

    Kriittisen iskun levelit viety attribuuteiksi omaan moduuliin

--- src/Damage/CriticalHit.hs
+++ src/Damage/CriticalHit.hs
@@ -21,30 +21,6 @@ import Data.Maybe
 
 data CriticalHit = NotCritical | IsCritical deriving (Eq,Show,Ord,Enum)
 
-data CriticalHitLevel =
-  NeverHits
-  | Level0
-  | Level1
-  | Level2
-  | Level3
-  | Level4
-  | AlwaysHits
-  deriving (Eq,Show,Ord,Enum)
-
-increaseCrit :: CriticalHitLevel -> CriticalHitLevel
-increaseCrit AlwaysHits = AlwaysHits
-increaseCrit x = succ x
-
-levelToInt :: CriticalHitLevel -> Int
-levelToInt = \case
-  NeverHits -> -1
-  Level0 -> 0
-  Level1 -> 1
-  Level2 -> 2
-  Level3 -> 3
-  Level4 -> 4
-  AlwaysHits -> 5
-
 type Prob = Double
 
 data CriticalHitCalc m = CriticalHitCalc
